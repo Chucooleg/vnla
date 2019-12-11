d05f5e17b50b0145ecde81eef9882e485f856d60
Chucooleg <chucooleg@gmail.com>
2019-12-11 18:25:17 +0000

    add philly environment variables for data and output directories

--- code/tasks/VNLA/scripts/define_vars.sh
+++ code/tasks/VNLA/scripts/define_vars.sh
@@ -3,5 +3,10 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-export PT_DATA_DIR=../../../data
-export PT_OUTPUT_DIR=output
+# local
+# export PT_DATA_DIR=../../../data
+# export PT_OUTPUT_DIR=output
+
+# relative to where train.py is
+export PT_DATA_DIR=/mnt/matterport3d/
+export PT_OUTPUT_DIR=/mnt/experiment-results-philly/output-philly
