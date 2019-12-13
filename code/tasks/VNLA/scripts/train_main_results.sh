33c296c70960534c9c7a9e34d26d0fec556bc455
Chucooleg <chucooleg@gmail.com>
2019-12-13 01:23:09 +0000

    update script to take in bootstrapping arguments

--- code/tasks/VNLA/scripts/train_main_results.sh
+++ code/tasks/VNLA/scripts/train_main_results.sh
@@ -8,35 +8,40 @@ source define_vars.sh
 cd ../
 
 exp_name=$1
-device=${2:-0}
+# extras=${2:-0}
+extras=${@:2}
 
 config_file="configs/verbal_hard.json"
 output_dir="main_$exp_name"
 
-extra=""
+ask=""
 
 if [ "$exp_name" == "none" ]
 then
-   extra="-no_ask 1"
+  ask="-no_ask 1"
 elif [ "$exp_name" == "first" ]
 then
-  extra="-ask_first 1"
+  ask="-ask_first 1"
 elif [ "$exp_name" == "random" ]
 then
-  extra="-random_ask 1"
+  ask="-random_ask 1"
 elif [ "$exp_name" == "teacher" ]
 then
-  extra="-teacher_ask 1"
+  ask="-teacher_ask 1"
 elif [ "$exp_name" == "learned" ]
 then
-  extra=""
+  ask=""
 else
-  echo "Usage: bash train_main_results.sh [none|first|random|teacher|learned] [gpu_id]"
-  echo "Example: bash train_main_results.sh learned 0"
+  # echo "Usage: bash train_main_results.sh [none|first|random|teacher|learned] [gpu_id]"
+  # echo "Example: bash train_main_results.sh learned 0"
+  echo "Usage: bash train_main_results.sh [none|first|random|teacher|learned] [extra arguments]"
+  echo "Example: bash train_main_results.sh learned -bootstrap 1 -n_ensemble 10 -bernoulli_probability 0.5 -bootstrap_majority_vote 1 -gradient_clipping 0"
   exit
 fi
 
-command="python -u train.py -config $config_file -exp $output_dir $extra -device $device"
+# command="python -u train.py -config $config_file -exp $output_dir $extra -device $device"
+# command="python -u train.py -config $config_file -exp $output_dir $ask $extras"
+command="python -u train.py -config $config_file -exp $output_dir $ask $extras"
 echo $command
 $command
 
