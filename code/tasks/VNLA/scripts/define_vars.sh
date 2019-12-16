7820f5de7512f83e3617d91238018490d40d79c4
Chucooleg <chucooleg@gmail.com>
2019-12-16 16:56:54 +0000

    update path to output dir as full path

--- code/tasks/VNLA/scripts/define_vars.sh
+++ code/tasks/VNLA/scripts/define_vars.sh
@@ -4,8 +4,8 @@
 # Licensed under the MIT license.
 
 # local
-# export PT_DATA_DIR=../../../data
-# export PT_OUTPUT_DIR=output
+# export PT_DATA_DIR=/home/hoyeung/blob_matterport3d/
+# export PT_OUTPUT_DIR=/home/hoyeung/Documents/vnla/code/tasks/VNLA/output/
 
 # relative to where train.py is
 export PT_DATA_DIR=/mnt/matterport3d/
